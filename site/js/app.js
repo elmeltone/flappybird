8f2d042e01540043b1bcfe4570c4acbb386b8485
MICHAEL J MELTON <meltonm83@gmail.com>
2016-04-08 14:12:25 -0700

    animate button

--- site/js/app.js
+++ site/js/app.js
@@ -1,3 +1,12 @@
 $(function() {
 	
+	$('#play').mouseenter(function(){
+		$(this)
+		.velocity({translateY:'-10px', backgroundColor:'#006400'}, 400);
+	});
+	$('#play').mouseleave(function(){
+		$(this)
+		.velocity({translateY:'0', backgroundColor:'#699864'}, 200);
+	});
+	
 });
